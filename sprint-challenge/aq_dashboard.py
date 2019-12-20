8ffb57c4654a2bafeec8c617f7dcfc5502c6e6ce
daanishrasheed <daanishrasheed@gmail.com>
2019-12-20 15:41:41 -0500

    part 2 is done (i hope)

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -1,10 +1,22 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
+from openaq import OpenAQ
+from flask_sqlalchemy import SQLAlchemy
 
 APP = Flask(__name__)
 
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+DB = SQLAlchemy(APP)
+
+api = OpenAQ()
 
 @APP.route('/')
 def root():
     """Base view."""
-    return 'TODO - part 2 and beyond!'
+    status, body = api.measurements(city='Los Angeles', parameter='pm25')
+    l = []
+    for x in body['results']:
+        value = x['value']
+        utc_datetime = x['date']['utc']
+        l.append((utc_datetime, value))
+    return str(l)
