f3c9ff9e53f1a3da971daea33a8852890b028e3e
daanishrasheed <daanishrasheed@gmail.com>
2019-12-20 16:12:00 -0500

    part 3 done

--- sprint-challenge/aq_dashboard.py
+++ sprint-challenge/aq_dashboard.py
@@ -20,3 +20,24 @@ def root():
         utc_datetime = x['date']['utc']
         l.append((utc_datetime, value))
     return str(l)
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return return '<datetime: {} --- value: {}>'.format(self.datetime, self.value)
+
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    d = root()
+    for res in d:
+        record = Record(datetime = res[0], value=res[1])
+        DB.session.add(record)
+    DB.session.commit()
+    return 'Data refreshed!'
